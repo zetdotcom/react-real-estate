912748e58398b4543fc242750ff19dd317bb8cbd
Michal Zet <michal.zadarnowski@gmail.com>
2018-02-09 22:00:41 +0000

    search box enabled

--- src/App.js
+++ src/App.js
@@ -1,181 +1,175 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from './assets/js/Header';
 import Filter from './assets/js/Filter';
 import Listings from './assets/js/Listings';
-import listingsData from './assets/js/data/listingsData.js'
+import listingsData from './assets/js/data/listingsData.js';
 import './assets/styles/main.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      listingsData,
-      city: 'All',
-      bedrooms: 0,
-      housetype: 'All',
-      min_price: 0,
-      max_price: 10000000,
-      min_floor_space: 0,
-      max_floor_space: 2000,
-      filteredData: listingsData,
-      populateFormsData: '',
-      sortby: 'price-dsc',
-      view: 'grid'
-    }
-    this.change = this
-      .change
-      .bind(this);
-    this.filteredData = this
-      .filteredData
-      .bind(this);
-    this.populateForms = this
-      .populateForms
-      .bind(this);
-    this.changeViewGrid = this
-      .changeViewGrid
-      .bind(this)
-    this.changeViewList = this
-      .changeViewList
-      .bind(this)
-  }
+	constructor() {
+		super();
+		this.state = {
+			listingsData,
+			city: 'All',
+			bedrooms: 0,
+			housetype: 'All',
+			min_price: 0,
+			max_price: 10000000,
+			min_floor_space: 0,
+			max_floor_space: 2000,
+			filteredData: listingsData,
+			populateFormsData: '',
+			sortby: 'price-dsc',
+			view: 'grid',
+			search: ''
+		};
+		this.change = this.change.bind(this);
+		this.filteredData = this.filteredData.bind(this);
+		this.populateForms = this.populateForms.bind(this);
+		this.changeViewGrid = this.changeViewGrid.bind(this);
+		this.changeViewList = this.changeViewList.bind(this);
+	}
 
-  componentWillMount() {
+	componentWillMount() {
+		let listingsData = this.state.listingsData.sort((a, b) => {
+			return a.price - b.price;
+		});
 
-    let listingsData = this
-      .state
-      .listingsData
-      .sort((a, b) => {
-        return a.price - b.price
-      })
+		this.changeViewGrid();
+		this.changeViewList();
+		this.setState({ listingsData });
+	}
 
-    this.changeViewGrid();
-    this.changeViewList();
-    this.setState({listingsData})
+	change(event) {
+		let name = event.target.name;
+		let value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+		this.setState(
+			{
+				[name]: value
+			},
+			() => {
+				console.log(this.state);
+				this.filteredData();
+			}
+		);
+	}
 
-  }
+	filteredData() {
+		let newData = this.state.listingsData.filter((item) => {
+			return (
+				item.price >= this.state.min_price &&
+				item.price <= this.state.max_price &&
+				item.space >= this.state.min_floor_space &&
+				item.space <= this.state.max_floor_space &&
+				item.rooms >= this.state.bedrooms
+			);
+		});
 
-  change(event) {
-    let name = event.target.name;
-    let value = (event.target.type === 'checkbox')
-      ? event.target.checked
-      : event.target.value;
-    this.setState({
-      [name]: value
-    }, () => {
-      console.log(this.state);
-      this.filteredData();
-    })
-  }
+		if (this.state.city !== 'All') {
+			newData = newData.filter((item) => {
+				return item.city === this.state.city;
+			});
+		}
 
-  filteredData() {
-    let newData = this
-      .state
-      .listingsData
-      .filter((item) => {
-        return item.price >= this.state.min_price && item.price <= this.state.max_price && item.space >= this.state.min_floor_space && item.space <= this.state.max_floor_space && item.rooms >= this.state.bedrooms
-      })
+		if (this.state.housetype !== 'All') {
+			newData = newData.filter((item) => {
+				return item.housetype === this.state.housetype;
+			});
+		}
 
-    if (this.state.city !== 'All') {
-      newData = newData.filter((item) => {
-        return item.city === this.state.city;
-      })
-    }
+		if (this.state.sortby === 'price-dsc') {
+			newData = newData.sort((a, b) => {
+				return a.price - b.price;
+			});
+		}
 
-    if (this.state.housetype !== 'All') {
-      newData = newData.filter((item) => {
-        return item.housetype === this.state.housetype;
-      })
-    }
+		if (this.state.sortby === 'price-asc') {
+			newData = newData.sort((a, b) => {
+				return b.price - a.price;
+			});
+		}
 
-    if (this.state.sortby === 'price-dsc') {
-      newData = newData.sort((a, b) => {
-        return a.price - b.price
-      })
-    }
+		if (this.state.search !== '') {
+			newData = newData.filter((item) => {
+				let city = item.city.toLowerCase();
+				let searchText = this.state.search.toLocaleLowerCase();
+				let n = city.match(searchText);
 
-    if (this.state.sortby === 'price-asc') {
-      newData = newData.sort((a, b) => {
-        return b.price - a.price
-      })
-    }
+				if (n !== null) {
+					return true;
+				}
+			});
+		}
 
-    this.setState({filteredData: newData})
-  }
+		this.setState({ filteredData: newData });
+	}
 
-  populateForms() {
-    //Cities
-    let cities = this
-      .state
-      .listingsData
-      .map((item) => {
-        return item.city
-      })
-    cities = new Set(cities);
-    cities = [...cities];
-    cities = cities.sort();
+	populateForms() {
+		//Cities
+		let cities = this.state.listingsData.map((item) => {
+			return item.city;
+		});
+		cities = new Set(cities);
+		cities = [ ...cities ];
+		cities = cities.sort();
 
-    //House Type
-    let houseTypes = this
-      .state
-      .listingsData
-      .map((item) => {
-        return item.housetype
-      })
-    houseTypes = new Set(houseTypes);
-    houseTypes = [...houseTypes];
-    houseTypes = houseTypes.sort();
+		//House Type
+		let houseTypes = this.state.listingsData.map((item) => {
+			return item.housetype;
+		});
+		houseTypes = new Set(houseTypes);
+		houseTypes = [ ...houseTypes ];
+		houseTypes = houseTypes.sort();
 
-    //bedrooms
-    let bedrooms = this
-      .state
-      .listingsData
-      .map((item) => {
-        return item.rooms
-      })
-    bedrooms = new Set(bedrooms);
-    bedrooms = [...bedrooms];
-    bedrooms = bedrooms.sort();
+		//bedrooms
+		let bedrooms = this.state.listingsData.map((item) => {
+			return item.rooms;
+		});
+		bedrooms = new Set(bedrooms);
+		bedrooms = [ ...bedrooms ];
+		bedrooms = bedrooms.sort();
 
-    this.setState({
-      populateFormsData: {
-        houseTypes,
-        bedrooms,
-        cities
-      }
-    }, () => {
-      console.log(this.state)
-    })
-  }
+		this.setState(
+			{
+				populateFormsData: {
+					houseTypes,
+					bedrooms,
+					cities
+				}
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
+	}
 
-  changeViewGrid() {
-    this.setState({view: 'grid'})
-    console.log(this.state.view)
-  }
+	changeViewGrid() {
+		this.setState({ view: 'grid' });
+		console.log(this.state.view);
+	}
 
-  changeViewList() {
-    this.setState({view: 'list'})
-    console.log(this.state.view)
-  }
+	changeViewList() {
+		this.setState({ view: 'list' });
+		console.log(this.state.view);
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <section id="content-area">
-          <Filter
-            change={this.change}
-            globalState={this.state}
-            populateAction={this.populateForms}/>
-          <Listings
-            listingsData={this.state.filteredData}
-            change={this.change}
-            globalState={this.state}
-            changeViewGrid={this.changeViewGrid}
-            changeViewList={this.changeViewList}/>
-        </section>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<section id="content-area">
+					<Filter change={this.change} globalState={this.state} populateAction={this.populateForms} />
+					<Listings
+						listingsData={this.state.filteredData}
+						change={this.change}
+						globalState={this.state}
+						changeViewGrid={this.changeViewGrid}
+						changeViewList={this.changeViewList}
+					/>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/assets/js/Listings.js
+++ src/assets/js/Listings.js
@@ -1,153 +1,135 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 class Listings extends Component {
-  constructor() {
-    super()
-    this.state = {
-      name: "michal"
-    }
+	constructor() {
+		super();
+		this.state = {
+			name: 'michal'
+		};
 
-    this.loopListings = this
-      .loopListings
-      .bind(this);
-    this.listingImageClass = this
-      .listingImageClass
-      .bind(this);
-    this.listingClass = this
-      .listingClass
-      .bind(this);
-    this.listingsResultsClass = this
-      .listingsResultsClass
-      .bind(this);
-  }
+		this.loopListings = this.loopListings.bind(this);
+		this.listingImageClass = this.listingImageClass.bind(this);
+		this.listingClass = this.listingClass.bind(this);
+		this.listingsResultsClass = this.listingsResultsClass.bind(this);
+	}
 
-  listingImageClass() {
-    let {view} = this.props.globalState;
-    return (view == 'grid')
-      ? 'listing-image-grid'
-      : 'listing-image-list'
-    console.log(view)
-  }
+	listingImageClass() {
+		let { view } = this.props.globalState;
+		return view == 'grid' ? 'listing-image-grid' : 'listing-image-list';
+		console.log(view);
+	}
 
-  listingClass() {
-    let {view} = this.props.globalState;
-    return (view == 'grid')
-      ? 'listing-grid'
-      : 'listing-list'
-    console.log(view)
-  }
+	listingClass() {
+		let { view } = this.props.globalState;
+		return view == 'grid' ? 'listing-grid' : 'listing-list';
+		console.log(view);
+	}
 
-  listingsResultsClass() {
-    let {view} = this.props.globalState;
-    let showGrid = {
-      flexFlow: 'row wrap'
-    }
-    let showList = {
-      flexFlow: 'column'
-    }
-    return (view == 'grid')
-      ? showGrid
-      : showList
+	listingsResultsClass() {
+		let { view } = this.props.globalState;
+		let showGrid = {
+			flexFlow: 'row wrap'
+		};
+		let showList = {
+			flexFlow: 'column'
+		};
+		return view == 'grid' ? showGrid : showList;
+	}
 
-  }
+	loopListings() {
+		const { listingsData } = this.props;
 
-  loopListings() {
-    const {listingsData} = this.props
+		if (listingsData === undefined || listingsData.length === 0) {
+			return 'Sorry no listings match your filters...';
+		}
 
-    if (listingsData === undefined || listingsData.length === 0) {
-      return "Sorry no listings match your filters..."
-    }
+		return listingsData.map((listing, index) => {
+			return (
+				<div className={this.listingClass()} key={index}>
+					<div
+						className={this.listingImageClass()}
+						style={{
+							background: `url("${listing.image}") center center / cover no-repeat`
+						}}
+					>
+						<div className="details">
+							<div className="user">
+								<div className="user-img" />
+								<div className="user-details">
+									<span className="user-name">Nina Smith</span>
+									<span className="post-date">05/01/2018</span>
+								</div>
+							</div>
+							<div className="listing-details">
+								<div className="floor-space">
+									<i className="fa fa-square-o" aria-hidden="true" />
+									<span>
+										{listing.space}
+										m&sup2;
+									</span>
+								</div>
+								<div className="bedrooms">
+									<i className="fa fa-bed" aria-hidden="true" />
+									<span>
+										{listing.rooms}
+										&nbsp; bedrooms
+									</span>
+								</div>
+							</div>
+						</div>
+						<span className="address">{listing.city}</span>
+					</div>
+					<div className="bottom-info">
+						<span>£{listing.price}</span>
+						<span>
+							<i className="fa fa-map-marker" aria-hidden="true" />
+							{listing.city}
+						</span>
+					</div>
+				</div>
+			);
+		});
+	}
 
-    return listingsData.map((listing, index) => {
-      return <div className={this.listingClass()} key={index}>
-        <div
-          className={this.listingImageClass()}
-          style={{
-          background: `url("${listing.image}") center center / cover no-repeat`
-        }}>
-          <div className='details'>
-            <div className='user'>
-              <div className='user-img'/>
-              <div className='user-details'>
-                <span className='user-name'>Nina Smith</span>
-                <span className='post-date'>05/01/2018</span>
-              </div>
-            </div>
-            <div className='listing-details'>
-              <div className='floor-space'>
-                <i className='fa fa-square-o' aria-hidden='true'/>
-                <span>{listing.space}
-                  m&sup2;</span>
-              </div>
-              <div className='bedrooms'>
-                <i className='fa fa-bed' aria-hidden='true'/>
-                <span>{listing.rooms}
-                  &nbsp; bedrooms</span>
-              </div>
-            </div>
-          </div>
-          <span className='address'>{listing.city}</span>
-        </div>
-        <div className='bottom-info'>
-          <span>£{listing.price}</span>
-          <span>
-            <i className='fa fa-map-marker' aria-hidden='true'/>{listing.city}
-          </span>
-        </div>
-      </div>
-    })
-  }
+	render() {
+		return (
+			<section id="listings">
+				<div className="search-and-sort">
+					<section className="search-area">
+						<input type="text" name="search" onChange={this.props.change} />
 
-  render() {
+						<div className="results">390 results found</div>
+					</section>
 
-    return (
+					<section className="sortby-area">
+						<div className="view">
+							<i className="fa fa-th-list" aria-hidden="true" onClick={this.props.changeViewList} />
+							<i className="fa fa-th" aria-hidden="true" onClick={this.props.changeViewGrid} />
+						</div>
+						<select name="sortby" className="sortby" onChange={this.props.change}>
+							<option value="price-dsc">Price: Low to High</option>
+							<option value="price-asc">Price: High to Low</option>
+						</select>
+					</section>
+				</div>
 
-      <section id='listings'>
+				<section className="listings-results" style={this.listingsResultsClass()}>
+					{this.loopListings()}
+				</section>
 
-        <div className='search-and-sort'>
-          <section className='search-area'>
-            <input type='text' name='search'/>
-
-            <div className='results'>390 results found</div>
-          </section>
-
-          <section className='sortby-area'>
-            <div className='view'>
-              <i
-                className='fa fa-th-list'
-                aria-hidden='true'
-                onClick={this.props.changeViewList}/>
-              <i className='fa fa-th' aria-hidden='true' onClick={this.props.changeViewGrid}/>
-            </div>
-            <select name='sortby' className='sortby' onChange={this.props.change}>
-              <option value='price-dsc'>Price: Low to High</option>
-              <option value='price-asc'>Price: High to Low</option>
-            </select>
-
-          </section>
-        </div>
-
-        <section className='listings-results' style={this.listingsResultsClass()}>
-
-          {this.loopListings()}
-
-        </section>
-
-        <section className='pagination'>
-
-          <ul className='pages'>
-            <li>Prev</li>
-            <li className='active'>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>Next</li>
-          </ul>
-        </section>
-      </section>
-
-    )
-  }
+				<section className="pagination">
+					<ul className="pages">
+						<li>Prev</li>
+						<li className="active">1</li>
+						<li>2</li>
+						<li>3</li>
+						<li>4</li>
+						<li>Next</li>
+					</ul>
+				</section>
+			</section>
+		);
+	}
 }
 
-export default Listings
+export default Listings;
