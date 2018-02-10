ad44bf4ab3d62fd06ee85c0fe46763b0bb16a487
Michal Zet <michal.zadarnowski@gmail.com>
2018-02-10 14:10:26 +0000

    responvice header styles

--- src/assets/js/Header.js
+++ src/assets/js/Header.js
@@ -1,23 +1,50 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import FaBars from 'react-icons/lib/fa/bars'
+import logo from '../images/logo.png';
 
 class Header extends Component {
-  render() {
-    return (
+		constructor() {
+				super();
+				this.state = {
+						menu: 'close',
+						menuBtnBars: true
+				}
 
-      <header>
-        <div className='logo'>Logo</div>
+				this.buttonChange = this
+						.buttonChange
+						.bind(this);
+		}
+		buttonChange() {
+				this.setState({
+						menuBtnBars: !this.state.menuBtnBars
+				})
+		}
+		render() {
 
-        <nav>
-          <a href='#'>Create Ads</a>
-          <a href='#'>About Us</a>
-          <a href='#'>Log In</a>
-          <a href='#' className='register-btn'>Register</a>
+				return (
+						<div className="header">
+								<div className="logo">
+										<img src={logo}/>
+										<p>REAL ESTATE</p>
+								</div>
 
-        </nav>
-      </header>
+								<div className="nav">
+										<div id="btn" className="menuBtn">
+												<FaBars/>
+										</div>
+										<div className="navMenu">
+												<a href="#">Create Ads</a>
+												<a href="#">About Us</a>
+												<a href="#">Log In</a>
+												<a href="#" className="register-btn">
+														Register
+												</a>
+										</div>
 
-    )
-  }
+								</div>
+						</div>
+				);
+		}
 }
 
-export default Header
+export default Header;
